68a850bdc0940608284dca04a247e7f8070dcc71
Alfredo Manuel <alfredochivela3@gmail.com>
2024-08-12 14:07:00 +0100

    updated

--- src/strings.c
+++ src/strings.c
@@ -1,5 +1,52 @@
 #include "42lib.h"
 
+char *rostring(char *str)
+{
+	char *word_start;
+	char *word_end;
+	char *res = (char *)malloc(char);
+	
+	while(*str == ' ' || *str == '\t')
+		str++;
+	word_start = str;
+	while(*str && *str != ' ' && *str != '\t')
+		str++;
+	word_end = str;
+	while(*str == ' ' || *str == '\t')
+		str++;
+	if(*str)
+	{
+		while(*str)
+		{
+			if(*str == ' ' || *str == '\t')
+			{
+				while(*str == ' ' || *str == '\t')
+					str++;
+				if(*str)
+					write(1," ",1);
+			}
+		}
+		write(1," ",1);
+	}
+}
+void first_word(char *str, char *res)
+{
+    int i = 0;
+    int len = 0;
+
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ' || str[i] == '\t')
+            break;
+        res[len] = str[i];
+        len++;
+        i++;
+    }
+    res[len] = '\0';
+}
+
 int ft_strlen(char *str)
 {
     int i =0;
