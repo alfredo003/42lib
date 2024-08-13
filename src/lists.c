eea516538647a40762e775ba67383465560d10fe
Alfredo Manuel <alfredochivela3@gmail.com>
2024-08-13 08:12:55 +0100

    updated

--- src/lists.c
+++ src/lists.c
@@ -6,3 +6,26 @@ int *ft_list_size(t_list *begin_list)
         return (0);
     return (1+ begin_list(begin_list->next));
 }
+
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+{
+	int swap;
+	
+	t_list *tmp;
+	
+	tmp =lst;
+	
+	while(lst != NULL && lst->next != NULL)
+	{
+		if((*cmp)(lst->data,lst->next->data) == 0)
+		{
+			swap = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = swap;
+			lst = tmp;
+		}
+		else
+			lst = lst->next;
+	}
+	return (tmp);
+}
